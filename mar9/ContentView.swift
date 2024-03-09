c9203df7c5c42baf4be872a6244ad77cd9e4b77d
smd-silverlineit <136455829+smd-silverlineit@users.noreply.github.com>
2024-03-09 10:57:17 +0530

    Initial Commit

---
+++ mar9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mar9
+//
+//  Created by SMD Thiranjaya on BE 2567-03-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mar9/mar9App.swift
@@ -0,0 +1,17 @@
+//
+//  mar9App.swift
+//  mar9
+//
+//  Created by SMD Thiranjaya on BE 2567-03-09.
+//
+
+import SwiftUI
+
+@main
+struct mar9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
